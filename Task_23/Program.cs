cb11d2939ef0c4f8f5ac24c4bb626b530b6b1f36
Viktor <abcd999@bk.ru>
2022-08-17 21:39:39 +0300

    Задача 23

---
+++ Task_23/Program.cs
@@ -0,0 +1,17 @@
+﻿//Выдает кубы чисел от 1 до N включительно
+Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N > 0)
+{
+    for (int a = 1; a <= N; a++)
+    {
+        Console.WriteLine();
+        Console.Write(Math.Pow(a, 3));
+    }
+}
+else for (int a = 1; a >= N; a--)
+    {
+        Console.WriteLine();
+        Console.Write(Math.Pow(a, 3));
+    }
+
